fbe6cc4ab51dd17d2ce9d6d1d949b173ead9bde6
爱学兮 <aixuexi567@icloud.com>
2025-04-01 12:35:27 +0800

    feat(about): enhance About page with back button and update styles

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,16 +1,22 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text, Button } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
+import "./index.scss";
 
 export default function About() {
   useLoad(() => {
-    console.log('About page loaded.')
-  })
+    console.log("About page loaded.");
+  });
 
   return (
-    <View className='about'>
-      <Text className='title'>About Us</Text>
-      <Text className='content'>This is the About page of our Taro application.</Text>
+    <View className="about">
+      <Text className="title">About Us</Text>
+      <Text className="content">
+        This is the About page of our Taro application.
+      </Text>
+      {/* add a button to back the index */}
+      <Button className="back" onClick={() => Taro.navigateBack()}>
+        Back to Home
+      </Button>
     </View>
-  )
-} 
+  );
+}
